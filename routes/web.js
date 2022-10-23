5db4ce89238a6c7b0f35e762538891b13144b937
Anish <powaranish74@gmail.com>
2022-10-23 11:05:37 +0530

    added-Status change Functionality

--- routes/web.js
+++ routes/web.js
@@ -3,7 +3,7 @@ const cartController = require("../app/http/controllers/customers/cartController
 const orderController = require("../app/http/controllers/customers/orderController");
 const AdminOrderController = require("../app/http/controllers/admin/orderController");
 const homeController = require("../app/http/controllers/homeController");
-
+const statusController = require("../app/http/controllers/admin/statusController");
 
 //middlewares
 
@@ -35,6 +35,9 @@ function initRoutes(app) {
   // Admin Routes
 
   app.get("/admin/orders", admin, AdminOrderController().index);
+
+  app.post("/admin/order/status", admin, statusController().update);
+  //
 }
 
 // for export
